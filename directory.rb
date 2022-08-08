c3f4d667b0e00cf31fdbead8c5ebad220c95a771
iniffur <jamesruffini97@gmail.com>
2022-08-08 22:20:22 +0100

    Added total student number

--- directory.rb
+++ directory.rb
@@ -1,13 +1,19 @@
-puts 'The students of Villains Academy'
-puts '-----------------------'
-puts 'Dr. Hannibal Lecter'
-puts 'Darth Vader'
-puts 'Nurse Ratched'
-puts 'Michael Corleone'
-puts 'Alex DeLarge'
-puts 'The Wicked Witch of the West'
-puts 'Terminator'
-puts 'Freddy Krueger'
-puts 'The Joker'
-puts 'Joffrey Baratheon'
-puts 'Norman Bates'
+#print student list
+puts("The students of Villains Academy")
+puts("-------------")
+puts("Dr. Hannibal Lecter")
+puts("Darth Vader")
+puts("Nurse Ratched")
+puts("Michael Corleone")
+puts("Alex DeLarge")
+puts("The Wicked Witch of the West")
+puts("Terminator")
+puts("Freddy Krueger")
+puts("The Joker")
+puts("Joffrey Baratheon")
+puts("Norman Bates")
+#print total student number
+print "Overall, we have "
+#print() doesnt add new line characters
+print 11
+puts " great students"
