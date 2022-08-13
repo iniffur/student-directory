bf570fb40e260fc111bdeca3e4afa21687f34bb5
iniffur <jamesruffini97@gmail.com>
2022-08-14 00:32:57 +0100

    Taking arguements from CL

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
     loop do
         print_menu   
-        process(gets.chomp)
+        process(STDIN.gets.chomp)
     end
 end
 
@@ -19,12 +19,12 @@ def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"#
 
-    name = gets.chomp
+    name = STDIN.gets.chomp
 
     while !name.empty? do
         @students << {name: name, cohort: :november}
         puts "Now we have #{@students.length} students"
-        name = gets.chomp
+        name = STDIN.gets.chomp
     end
 end
 
@@ -77,15 +77,27 @@ def save_students
     file.close
 end
 
-def load_students
-    file = File.open("students.csv", "r")
-    
-    file.readLines.each do |line|
+def load_students(filename = "students.csv")
+    file = File.open(filename, "r")
+
+    file.readlines.each do |line|
         name, cohort = line.chomp.split(",")
         @students << {name: name, cohort: cohort.to_sym}
     end
     file.close
 end
 
-#call methods
+def try_load_students
+    filename = ARGV.first # first arguement from CL
+    return if filename.nil?
+    if File.exist?(filename)
+        load_students(filename)
+        puts "Loaded #{@students.length} from #{filename}"
+    else
+        puts "Sorry #{filename} does not exist"#
+        exit
+    end
+end
+
+try_load_students
 interactive_menu
