dbab2e4be79585f2da3bffd2d6e1eee83d665dd1
iniffur <jamesruffini97@gmail.com>
2022-08-14 00:12:38 +0100

    Saving data to a file

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ end
 def print_menu
     puts "press 1 to Input students"
     puts "press 2 to show the students"
+    puts "press 3 to save the list to students.csv"
     puts "press 9 to exit"
 end
 
@@ -38,6 +39,8 @@ def process(user_choice)
             students = input_students
         when "2"
             show_students
+        when "3"
+            save_students
         when "9"
             exit
         else 
@@ -58,6 +61,17 @@ end
 
 def print_footer
     puts "Overall, we have #{@students.length} great students"
+end
+
+def save_students
+    file = File.open("students.csv", "w")
+
+    @students.each do|student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(", ")
+        file.puts csv_line
+    end
+    file.close
 end
 
 #call methods
